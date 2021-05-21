395dfc164b39b84da5b5ffc89f50c8d64c847a83
Zoe Woo <zoewoo@MacBook-Pro-8.local>
2021-05-21 18:40:43 -0500

    upload project

---
+++ src/main/java/com/sded/landmanagement/LandmanagementApplication.java
@@ -0,0 +1,13 @@
+package com.sded.landmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LandmanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LandmanagementApplication.class, args);
+	}
+
+}
